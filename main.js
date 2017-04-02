20fa700c8c5fc623b8adf47d8be335992f23becc
ddsheard <ddsheard@gmail.com>
2017-04-02 14:41:31 -0400

    content now showing on index pg now to work on images

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
 // https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1
 
-// let classGitHub = ['conshus', 'tyjdavis', 'chrislebbano', 'filusmarvin', 'wjwjr', 'ddsheard', 'ochanya', 'jcr08'];
-
-
-    // var source   = document.querySelector("#movieListing-template").innerHTML;
-    // var template = Handlebars.compile(source);
-    // let moviePromises = classGitHub.map(user => {
-    //   return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`)
-    //    .then(object => object.json())
-    //    .then(object => console.log(object))
-    // })
-    // Promise.all(moviePromises).then(placeInTemplate);
-    //
-    // function placeInTemplate (movieUsers) {
-    //   var html   = movieUsers.map(user => template(user)).join('');
-    //   //the original, when context was just an object...
-    //   //var html = template(context);
-    //   var destination = document.querySelector('.handlebars-demo');
-    //   destination.innerHTML = html;
-    // }
+    function placeInTemplate (movieUsers) {
+      var html   = movieUsers.results.map(user => template(user)).join('');
+      //the original, when context was just an object...
+      //var html = template(context);
+      var destination = document.querySelector('.handlebars-demo');
+      destination.innerHTML = html;
+    }
     // the html array will get joined by commas, unless you have the call to .join('') at the end
 
-  
+// ==============================
+let source = document.querySelector('#movieListing-template').innerHTML;
+let template = Handlebars.compile(source);
+
     fetch (`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`)
     .then (response => response.json())
-    .then(object => console.log(object))
+    .then(placeInTemplate)
+    // .then(object => console.log(object))
