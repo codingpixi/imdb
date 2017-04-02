6ed63ee89be459a3c8de4c4c1977e85b5c27a9e8
ddsheard <ddsheard@gmail.com>
2017-04-02 17:57:22 -0400

    update to push

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 // https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1
 
-    function placeInTemplate (movieUsers) {
-      var html   = movieUsers.map(user => template(user)).join('');
-      //the original, when context was just an object...
-      //var html = template(context);
-      var destination = document.querySelector('.handlebars-demo');
-      destination.innerHTML = html;
-    }
-    // the html array will get joined by commas, unless you have the call to .join('') at the end
+function placeInTemplate(movieUsers) {
+    var html = movieUsers.map(user => template(user)).join('');
+    //the original, when context was just an object...
+    //var html = template(context);
+    var destination = document.querySelector('.handlebars-demo');
+    destination.innerHTML = html;
+}
+// the html array will get joined by commas, unless you have the call to .join('') at the end
 
 // ==============================
 let source = document.querySelector('#movieListing-template').innerHTML;
 let template = Handlebars.compile(source);
 
-    fetch (`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`)
+fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`)
     .then(response => response.json())
     .then(response => response.results.map(getMovieId))
     .then(waitPromises => Promise.all(waitPromises))
     .then(placeInTemplate);
-    //.then(object => console.log(object))
+//.then(object => console.log(object))
 
-function getMovieId (object){
-  console.log(object.id);
+function getMovieId(object) {
+    console.log(object.id);
 
-  return fetch(`https://api.themoviedb.org/3/movie/${object.id}?api_key=${key}&language=en-US`)
-  .then(response => response.json())
-  // .then(object => console.log(object))
+    return fetch(`https://api.themoviedb.org/3/movie/${object.id}?api_key=${key}&language=en-US`)
+        .then(response => response.json())
+    // .then(object => console.log(object))
 }
