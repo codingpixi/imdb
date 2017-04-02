0e0b955d913194288a7d1c9b6dca11ebcd41c9f6
ddsheard <ddsheard@gmail.com>
2017-04-02 17:21:47 -0400

    update files

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1
 
     function placeInTemplate (movieUsers) {
-      var html   = movieUsers.results.map(user => template(user)).join('');
+      var html   = movieUsers.map(user => template(user)).join('');
       //the original, when context was just an object...
       //var html = template(context);
       var destination = document.querySelector('.handlebars-demo');
@@ -14,6 +14,16 @@ let source = document.querySelector('#movieListing-template').innerHTML;
 let template = Handlebars.compile(source);
 
     fetch (`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`)
-    .then (response => response.json())
-    .then(placeInTemplate)
-    // .then(object => console.log(object))
+    .then(response => response.json())
+    .then(response => response.results.map(getMovieId))
+    .then(waitPromises => Promise.all(waitPromises))
+    .then(placeInTemplate);
+    //.then(object => console.log(object))
+
+function getMovieId (object){
+  console.log(object.id);
+
+  return fetch(`https://api.themoviedb.org/3/movie/${object.id}?api_key=${key}&language=en-US`)
+  .then(response => response.json())
+  // .then(object => console.log(object))
+}
